3a4d2e6b80978f825158170cd6f36c9536ed03c7
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2023-09-06 12:18:03 -0500

    adding users app

---
+++ cride/users/apps.py
@@ -0,0 +1,9 @@
+""" Users app """
+
+from django.apps import AppConfig
+
+class UsersAppConfig(AppConfig):
+    """Users app config."""
+
+    name = 'cride.users'
+    verbose_name = 'Users'
--- cride/utils/models.py
+++ cride/utils/models.py
@@ -3,24 +3,24 @@
 from django.db import models
 
 class CRideModel(models.Model):
-  """Comparte Ride base model.
-  Abstract class that includes configurations common to all the models in the project.
-  """
-  created = models.DateTimeField(
-    'created at',
-    auto_now_add=True,
-    help_text='Date time on which the object was created.'
-  )
+    """Comparte Ride base model.
+    Abstract class that includes configurations common to all the models in the project.
+    """
+    created = models.DateTimeField(
+        'created at',
+        auto_now_add=True,
+        help_text='Date time on which the object was created.'
+    )
 
-  modified = models.DateTimeField(
-    'modified at',
-    auto_now=True,
-    help_text='Date time on which the object was last modified.'
-  )
+    modified = models.DateTimeField(
+        'modified at',
+        auto_now=True,
+        help_text='Date time on which the object was last modified.'
+    )
 
-  class Meta:
-    """Meta option."""
-    abstract = True
+    class Meta:
+        """Meta option."""
+        abstract = True
 
-    get_latest_by = 'created'
-    ordering = ['-created', '-modified']
+        get_latest_by = 'created'
+        ordering = ['-created', '-modified']
