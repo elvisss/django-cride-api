dc2eafb5db18f62e58aecd2bd21b9a87c9d4f122
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2023-09-06 00:31:20 -0500

    adding abstract base model

---
+++ cride/utils/models.py
@@ -0,0 +1,26 @@
+"""Django models utilities"""
+
+from django.db import models
+
+class CRideModel(models.Model):
+  """Comparte Ride base model.
+  Abstract class that includes configurations common to all the models in the project.
+  """
+  created = models.DateTimeField(
+    'created at',
+    auto_now_add=True,
+    help_text='Date time on which the object was created.'
+  )
+
+  modified = models.DateTimeField(
+    'modified at',
+    auto_now=True,
+    help_text='Date time on which the object was last modified.'
+  )
+
+  class Meta:
+    """Meta option."""
+    abstract = True
+
+    get_latest_by = 'created'
+    ordering = ['-created', '-modified']
