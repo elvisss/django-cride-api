db34f2722552d787439782c74dd79877bb6723c6
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2023-09-11 22:35:31 -0500

    adding searching and ordering

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -2,10 +2,13 @@
 
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from cride.circles.serializers import CircleModelSerializer
 from cride.circles.permissions import isCircleAdmin
 from cride.circles.models import Circle, Membership
+
 
 class CircleViewSet(mixins.CreateModelMixin,
                     mixins.RetrieveModelMixin,
@@ -16,6 +19,13 @@ class CircleViewSet(mixins.CreateModelMixin,
 
     serializer_class = CircleModelSerializer
     lookup_field = 'slug_name'
+
+    # filters
+    filter_backends = (SearchFilter, OrderingFilter, DjangoFilterBackend)
+    search_fields = ('slug_name', 'name')
+    ordering_fields = ('rides_offered', 'rides_taken', 'name', 'created', 'member_limit')
+    ordering = ('-members__count', '-rides_offered', '-rides_taken')
+    filter_fields = ('verified', 'is_public')
 
     def get_queryset(self):
         """Restrict list to public-only."""
